6efc8bdf523dda13ebda2c96e495d0bde8b551cd
dongrerohan421 <dongrerohan421@gmail.com>
2020-02-01 14:22:57 -0500

    create JPA repository

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.Objects;
-
 @RestController
 public class CurrencyExchangeController {
 
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private ExchangeValueRepository exchangeValueRepository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public ExchangeValue  retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
-        exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
         return exchangeValue;
     }
 }
---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+     ExchangeValue findByFromAndTo(String from, String to);
+}
